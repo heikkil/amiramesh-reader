364d48e37e4119e7ab790a20dcbcf4c151db9794
Heikki Lehvaslaiho <heikki.lehvaslaiho@gmail.com>
2014-03-17 15:11:46 +0300

    Refactored IO code into its own class

--- bin/read_ameramesh.py
+++ bin/read_ameramesh.py
@@ -4,32 +4,6 @@ import re
 from pprint import pprint
 
 #
-# argument parsing using argparse
-#
-parser = argparse.ArgumentParser()
-parser.add_argument('positional_args',
-                    nargs='?',
-                    metavar='ARG')
-parser.add_argument('--file', '-f',
-                    dest='file')
-parser.add_argument('--verbose', '-v',
-                    dest='verbose',
-                    action='store_true')
-args = parser.parse_args()
-myfile = args.file
-verbose = args.verbose
-
-# if no file as option, first argument has to be the file
-if myfile == None:
-    myfile = args.positional_args
-
-# The flags -h and --help and the usage message are generated automatically.
-# Positional arguments are in args.positional_args
-# Options can follow positional arguments.
-## print args
-
-
-#
 # 3D point class
 #
 
@@ -47,8 +21,7 @@ class Node(object):
 
 
 class Point3D(Node):
-    """3D Point class with public x,y,z attributes and optional set of diameters
-    """
+    """3D Point class with public x,y,z attributes and optional set of diameters """
  
     def __init__(self, x=0, y=0, z=0):
         self.x = x
@@ -87,7 +60,8 @@ class Segment(object):
 #
 
 class Skeleton(object):
-    """nodes, segments, and their locations """
+    """Top storage object for a skeleton that knows about
+    nodes, segments, and their locations """
  
     def __init__(self):
         self.nodes = {}
@@ -119,15 +93,108 @@ class Skeleton(object):
         for s in self.segments:
              c+= len(s.points)
         print "Points   : " + str(c)
-            
+
+
+class AmirameshReader(object):
+    """ Read from a filehandle, parse, return a Skeleton object"""
+
+    def parse(self, f):
+
+        skel = Skeleton()       # storage object
+        points = []             # list of points
+        counter = 0             # section counter
+        linecounter = 0         # within sections
+
+        for line in f:
+            # trim white space, including \r,\n
+            line = line.strip()
+
+            # ignore empty lines
+            if not line:
+                continue
+
+            # skip intro
+            header = re.search('^@', line)
+            if counter == 0 and not header:
+                continue
+
+            # header
+            if header:
+                counter+= 1
+                linecounter = 0
+                continue
+
+            if counter == 1:            # nodes
+                match = re.search('([\d\.e\+\-]+) ([\d\.e\+\-]+) ([\d\.e\+\-]+)', line)
+                x,y,z = match.groups()
+                n = Node(x,y,z)
+                skel.add_node(linecounter,n)
+                linecounter += 1
+
+            elif counter == 2:          # segments to nodes
+                match = re.search('(\d+) (\d+)', line)
+                start,end = match.groups()
+                seg = Segment(start, end)
+                #print type(skel), type(skel.add_segment)
+                skel.add_segment(seg)
+        
+            elif counter == 3:          # point count within segment
+                match = re.search('(\d+)', line)
+                count = match.groups()
+                skel.segments[linecounter].pointcount = count
+                linecounter += 1
+
+            elif counter == 4:          # point coordinates within a segment
+                match = re.search('([\d\.e\+\-]+) ([\d\.e\+\-]+) ([\d\.e\+\-]+)', line)
+                x,y,z = match.groups()
+                p = Point3D(x,y,z)
+                #skel.add_point(linecounter, p)
+                points.append(p)
+                #linecounter += 1
+
+            elif counter > 4:           # one or more diameters
+                # empty values replaced by 0
+                if line == 'nan':
+                    line = '0'
+
+                match = re.search('([\d\.e\+\-]+)', line)
+                dia = match.groups()
+                points[linecounter].add_diameter(dia)
+                linecounter += 1
+                
+        # add points in the end for efficiency
+        skel.add_points(points)
+        return skel
+
 #
 # main
 #
 
+#
+# argument parsing using argparse
+#
+parser = argparse.ArgumentParser()
+parser.add_argument('positional_args',
+                    nargs='?',
+                    metavar='ARG')
+parser.add_argument('--file', '-f',
+                    dest='file')
+parser.add_argument('--verbose', '-v',
+                    dest='verbose',
+                    action='store_true')
+args = parser.parse_args()
+myfile = args.file
+verbose = args.verbose
 
-points = []                     # list of points
-counter = 0                     # section counter
-linecounter = 0                 # within sections
+# if no file as option, first argument has to be the file
+if myfile == None:
+    myfile = args.positional_args
+
+# The flags -h and --help and the usage message are generated automatically.
+# Positional arguments are in args.positional_args
+# Options can follow positional arguments.
+## print args
+
 
 try:
     f = open(myfile)
@@ -135,69 +202,8 @@ except (TypeError, IOError):
     print "File not given or does not exist: ", myfile
     exit(1)
 
-skel = Skeleton()                 # storage object
-    
-for line in f:
-    # trim white space, including \r,\n
-    line = line.strip()
-
-    # ignore empty lines
-    if not line:
-        continue
-
-    # skip intro
-    header = re.search('^@', line)
-    if counter == 0 and not header:
-        continue
-
-    # header
-    if header:
-        counter+= 1
-        linecounter = 0
-        continue
-
-    if counter == 1:            # nodes
-        match = re.search('([\d\.e\+\-]+) ([\d\.e\+\-]+) ([\d\.e\+\-]+)', line)
-        x,y,z = match.groups()
-        n = Node(x,y,z)
-        skel.add_node(linecounter,n)
-        linecounter += 1
-
-    elif counter == 2:          # segments to nodes
-        match = re.search('(\d+) (\d+)', line)
-        start,end = match.groups()
-        seg = Segment(start, end)
-        #print type(skel), type(skel.add_segment)
-        skel.add_segment(seg)
-        
-    elif counter == 3:          # point count within segment
-        match = re.search('(\d+)', line)
-        count = match.groups()
-        skel.segments[linecounter].pointcount = count
-        linecounter += 1
-
-    elif counter == 4:          # point coordinates within a segment
-        match = re.search('([\d\.e\+\-]+) ([\d\.e\+\-]+) ([\d\.e\+\-]+)', line)
-        x,y,z = match.groups()
-        p = Point3D(x,y,z)
-        #skel.add_point(linecounter, p)
-        points.append(p)
-        #linecounter += 1
-
-    elif counter > 4:           # one or more diameters
-        # empty values replaced by 0
-        if line == 'nan':
-            line = '0';
-        match = re.search('([\d\.e\+\-]+)', line)
-
-        dia = match.groups()
-        points[linecounter].add_diameter(dia)
-        linecounter += 1
-
-    #print line
-        
-# add points in the end for efficiency
-skel.add_points(points)
+reader = AmirameshReader()
+skel = reader.parse(f)
 
 if verbose:
     skel.info()
@@ -210,4 +216,4 @@ if verbose:
 
 #pprint(skel.__dict__)
 #pprint(skel.segments[0].__dict__)
-#pprint(points[0].__dict__)
+#pprint(skel.segments[0].points[0].__dict__)
